68472a94a713cbca849e8c749488da7d3a60791a
watermelonwolverine <29666253+watermelonwolverine@users.noreply.github.com>
2022-04-10 16:03:26 +0200

    Removed readme from whl file.

--- setup.py
+++ setup.py
@@ -9,19 +9,12 @@ from cli_wrapper.__constants import app_name, author, url, issues_url
 # noinspection PyPep8
 from fvttmv import __version__
 
-long_description: str
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name=app_name,
     version=__version__,
     author=author,
     author_email="29666253+watermelonwolverine@users.noreply.github.com",
     description="Moves files while also updating FoundryVTT databases",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url=url,
     project_urls={
         "Bug Tracker": issues_url,
